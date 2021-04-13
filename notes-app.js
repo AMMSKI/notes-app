8e17ab212549de79fafdfb55d976a36af43c5a89
AMMSKI <73558293+AMMSKI@users.noreply.github.com>
2021-04-13 09:08:23 -0600

    Update notes-app.js

--- notes-app.js
+++ notes-app.js
@@ -34,3 +34,19 @@ window.addEventListener('storage', function(e){
     }
 })
 
+//Unix Epoch - January 1st 1970 00:00:00
+
+const now = new Date()
+const timestamp = now.getTime()
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+
+// console.log(now.toString())
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of the month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minute: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
