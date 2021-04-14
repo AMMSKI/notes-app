e2bc0bfea0307415bd410ea9a69a519f7478aa48
AMMSKI <73558293+AMMSKI@users.noreply.github.com>
2021-04-14 16:54:21 -0600

    Adding Moment Library

--- notes-app.js
+++ notes-app.js
@@ -34,19 +34,10 @@ window.addEventListener('storage', function(e){
     }
 })
 
-//Unix Epoch - January 1st 1970 00:00:00
-
-const now = new Date()
-const timestamp = now.getTime()
-
-const myDate = new Date(timestamp)
-console.log(myDate.getFullYear())
-
-// console.log(now.toString())
-
-// console.log(`Year: ${now.getFullYear()}`)
-// console.log(`Month: ${now.getMonth()}`)
-// console.log(`Day of the month: ${now.getDate()}`)
-// console.log(`Hour: ${now.getHours()}`)
-// console.log(`Minute: ${now.getMinutes()}`)
-// console.log(`Seconds: ${now.getSeconds()}`)
+const now = moment()
+//now.subtract(1, 'year').subtract(43, 'minutes')
+console.log(now.toString())
+console.log(now.format('MMMM Do, YYYY'))
+console.log(now.fromNow())
+const nowTimeStamp = now.valueOf()
+console.log(moment(nowTimeStamp).toString())
